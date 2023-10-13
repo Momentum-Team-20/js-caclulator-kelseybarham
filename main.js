d15093d96bf030e958acae30680064e8c777cca1
Kelsey Barham <kelseylbarham@gmail.com>
2023-10-13 14:22:21 -0400

    made body of calculator, needs javascript

---
+++ main.js
@@ -0,0 +1,31 @@
+console.log('connected')
+
+/*
+This function takes a number
+and inserts it into the display in the calculator
+*/
+let buttonPress = function(number) {
+    console.log(number)
+/**
+ * 1. Grab the display element
+ * 2. Create a new text element that contains number
+ * 3. prepend the text element to the display element
+ */
+
+const displayContainer = document.querySelector('#displayscreen')
+console.log(displayContainer)
+let numberContainer = document.createElement('div')
+let numberText = document.createTextNode(number)
+console.log(numberContainer)
+console.log(numberText)
+numberContainer.appendChild(numberText)
+console.log(numberContainer)
+displayContainer.prepend(numberContainer)
+console.log(displayContainer)
+
+}
+
+
+
+buttonPress(9);
+
