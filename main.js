3e1c71f18a7e0675e7e62b85a199df6e4d1150e3
Kelsey Barham <kelseylbarham@gmail.com>
2023-10-15 16:07:47 -0400

    operators function, clear butt functions, equals sign function

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 console.log('connected')
+const userInputs = []
 
 /*
 This function takes a number
 and inserts it into the display in the calculator
 */
-let buttonPress = function(number) {
-    console.log(number)
+// save a function (a doer) that takes an argument (input) of number and is called createTextOnDisplay 
+let updateDisplay = function(textToUpdateDisplay) {
+    console.log(textToUpdateDisplay)
 /**
  * 1. Grab the display element
  * 2. Create a new text element that contains number
  * 3. prepend the text element to the display element
- */
+*/
 
 const displayContainer = document.querySelector('#displayscreen')
 console.log(displayContainer)
 let numberContainer = document.createElement('div')
-let numberText = document.createTextNode(number)
+let numberText = document.createTextNode(textToUpdateDisplay)
 console.log(numberContainer)
 console.log(numberText)
 numberContainer.appendChild(numberText)
@@ -30,10 +32,39 @@ console.log(numbers)
 
 for (let number of numbers) {
     number.addEventListener('click', (event) => {
-        console.log(event.target.innerText)
-    buttonPress(number.innerText)
+    if(userInputs.length === 0) {
+        const displayContainer = document.querySelector('#displayscreen')
+        displayContainer.innerHTML = ""
+    }
+    console.log(event.target.innerText)
+    updateDisplay(event.target.innerText)
+    userInputs.push(event.target.innerText)
 })
 }
 
+let operators = document.querySelectorAll('.operator')
+console.log(operators)
 
+for (let operator of operators) {
+    operator.addEventListener('click', (event) => {
+        console.log(event.target.innerText)
+        updateDisplay(event.target.innerText)
+        userInputs.push(event.target.innerText)
+    })
+}
 
+let equals = document.querySelector('.equals')
+equals.addEventListener('click', (event) => {
+console.log(eval(userInputs.join("")))
+const displayContainer = document.querySelector('#displayscreen')
+displayContainer.innerHTML = ""
+updateDisplay(eval(userInputs.join("")))
+userInputs.length = 0;
+})
+
+let clear = document.querySelector('.clear')
+clear.addEventListener('click', (event) => {
+    const displayContainer = document.querySelector('#displayscreen')
+    displayContainer.innerHTML = ""
+    userInputs.length = 0;
+})
